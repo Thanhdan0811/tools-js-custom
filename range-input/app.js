470131404e4047b8b97093023f9efc2ce68a363c
thanhdan0811 <ngthdan0811@gmail.com>
2021-05-27 23:11:43 +0700

    chinh code

--- range-input/app.js
+++ range-input/app.js
@@ -1,29 +1,35 @@
-function rangeInput(selectorObj = {selector: "", min: 1, max: 100, step: 1} || "") {
-    console.log(selectorObj)
-    let {selector, min, max, step} = selectorObj;
+function rangeInput(selector, obj) {
     if(!selector) {
         return;
     }
     if(!document.querySelector(selector)) return;
+    console.log("obj", obj);
     let slider = document.querySelector(selector);
-    setUpClass();
-    let range = slider.querySelector(".slider-range");
-    let handle = slider.querySelector(".slider-handle");
-    let leftSlider = parseInt(slider.getBoundingClientRect().left);
-    let rightSlider = parseInt(slider.getBoundingClientRect().right);
-    let widthSlider = parseInt(slider.getBoundingClientRect().width);
+    let {range,handle,leftSlider,rightSlider,widthSlider} = setUpClass();
+    let {min = 0, max = 100, step = 1, value = 50} = obj;
     // let min = 0;
     // let max = 500;
     // let step = 50;
-    let value = 100;
+    // let value = 100;
+
+
     function setUpClass() {
         slider.classList.add("slider");
-        let content = `
+        let content = ` 
                         <div class="slider-range"></div>
                         <span class="slider-handle"></span>
                     `;
         slider.innerHTML = content;
+        return {
+            range : slider.querySelector(".slider-range"),
+            handle : slider.querySelector(".slider-handle"),
+            leftSlider : parseInt(slider.getBoundingClientRect().left),
+            rightSlider : parseInt(slider.getBoundingClientRect().right),
+            widthSlider : parseInt(slider.getBoundingClientRect().width),
+        }
     }
+
+
     function comparedCor(corX) {
         let checkLeft = +corX >= +leftSlider ? true : false;
         let checkRight = +corX <= +rightSlider ? true : false;
@@ -37,21 +43,15 @@ function rangeInput(selectorObj = {selector: "", min: 1, max: 100, step: 1} || "
         let checkRight = +corX > +rightSlider ? true : false;
         return checkRight;
     }
+
+
     function mouseHandle(clientX) {
         let shift = clientX - leftSlider;
         if(shift <= 0 ) {
-            document.querySelector(".value").innerHTML = min;
-            document.querySelector(".percent").innerHTML = 0 ;
-            handle.style.left = 0 + "%";
-            range.style.width = 0 + "%";
-            return;
+            shift = 0;
         }
         if(shift >= widthSlider) {
-            document.querySelector(".value").innerHTML = max;
-            document.querySelector(".percent").innerHTML = 100 ;
-            handle.style.left = 100 + "%";
-            range.style.width = 100 + "%";
-            return;
+            shift = widthSlider;
         }
         let percentShift = 100 * shift / widthSlider ;
         let numberOfRange = (max - min) % step == 0 ? (max - min) / step : Number.parseInt( (max - min) / step) + 1;
@@ -65,15 +65,18 @@ function rangeInput(selectorObj = {selector: "", min: 1, max: 100, step: 1} || "
 
         handle.style.left = percentCurrent + "%";
         range.style.width = percentCurrent + "%";
+        return {
+            currentValue,
+        }
     }
+
+
     function mouseDown(e) {
         handle.ondragstart = function() {return false};
         function mouseMove(e) {
-            if(comparedCor(e.clientX)) {
-                mouseHandle(e.clientX)
-                return;
-            }
-            mouseHandle(e.clientX);
+            let {currentValue} = mouseHandle(e.clientX);
+            sliderCallback();
+            
         }
         function mouseUp(e) {
             document.onmousemove = null;
@@ -83,6 +86,18 @@ function rangeInput(selectorObj = {selector: "", min: 1, max: 100, step: 1} || "
         document.onmousemove = mouseMove;
         document.onmouseup = mouseUp;
     }
+
+
     handle.onmousedown = mouseDown;
+    function sliderCallback() {
+        console.log("after call");
+    }
+    return {
+
+    }
 }
-rangeInput("#slider-custom");
+// rangeInput.prototype.setUpClass
+
+window.rangeInput = rangeInput;
+window.rangeInput("#slider-custom", "");
+
